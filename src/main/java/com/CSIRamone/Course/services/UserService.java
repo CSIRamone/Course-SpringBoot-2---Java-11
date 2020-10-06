f01042f119d38782baae58f1e29876928582aa1a
CSIRamone <cidinei_inacio@hotmail.com>
2020-10-05 21:21:16 -0300

    Service layer, component registration

---
+++ src/main/java/com/CSIRamone/Course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.CSIRamone.Course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.CSIRamone.Course.entities.User;
+import com.CSIRamone.Course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
